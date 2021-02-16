f39ff5bd0ad19e149ab08944aed8786b49a466fd
Panos Tolios <panostolios@gmail.com>
2021-02-16 14:36:08 +0000

    Initial setup of API endpoint

--- src/api/tests.py
+++ src/api/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+import json
+from pprint import pprint
 
-# Create your tests here.
+
+class TestApi(TestCase):
+    def test_api_response(self):
+        client = Client()
+        post_data = {
+            "data": [
+                "scene",
+                "uncle",
+                "tale",
+                "aspect",
+                "disease",
+                "penalty",
+                "procedure",
+                "agreement",
+                "relationship",
+                "grocery"
+            ]
+        }
+        response = client.post("/api/sort/", post_data)
+        self.assertEqual(sorted(post_data["data"]), response.json())
--- src/api/views.py
+++ src/api/views.py
@@ -1,3 +1,14 @@
+import time
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def api(request):
+    if request.method == "POST":
+        result = request.POST.getlist("data")
+        print("Waiting....")
+        time.sleep(2)
+        print("Go!")
+        return JsonResponse(sorted(result), safe=False)
+    else:
+        return HttpResponse("No GET data")
