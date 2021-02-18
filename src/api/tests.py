e696923532164799d09c03a966d943502b8035b7
Panos Tolios <panostolios@gmail.com>
2021-02-18 13:46:58 +0000

    API validation & test

--- src/api/tests.py
+++ src/api/tests.py
@@ -1,4 +1,5 @@
 import json
+from typing import Dict, List, Union
 from django.test import TestCase
 
 from utils import quick_sort
@@ -6,7 +7,7 @@ from utils import quick_sort
 
 class TestApi(TestCase):
     def test_api_response(self):
-        post_data = [
+        post_data: List = [
             "scene",
             "uncle",
             "tale",
@@ -23,10 +24,47 @@ class TestApi(TestCase):
         )
         self.assertEqual(sorted(post_data), response.json())
 
+    def test_post_invalid_data(self):
+        post_data: Dict[str, List] = {
+            "data": [
+                "scene",
+                "uncle",
+                "tale",
+                "aspect",
+                "disease",
+                "penalty",
+                "procedure",
+                "agreement",
+                "relationship",
+                "grocery",
+            ]
+        }
+        response = self.client.post(
+            "/api/sort/", post_data, content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 400)
+
+        post_data: List[Union[str, int, float]] = [
+            "scene",
+            "uncle",
+            "tale",
+            "aspect",
+            "disease",
+            654,  # integer
+            "procedure",
+            "agreement",
+            324.84,  # float
+            "grocery",
+        ]
+        response = self.client.post(
+            "/api/sort/", post_data, content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 400)
+
 
 class TestQuickSort(TestCase):
     def test_quicksort(self):
-        original: list = [
+        original: List = [
             "scene",
             "uncle",
             "tale",
@@ -38,6 +76,6 @@ class TestQuickSort(TestCase):
             "relationship",
             "grocery",
         ]
-        sorted_list: list = original.copy()
+        sorted_list: List = original.copy()
         sorted_list = quick_sort(sorted_list, 0, len(sorted_list) - 1)
         self.assertEqual(sorted(original), sorted_list)
--- src/api/views.py
+++ src/api/views.py
@@ -1,6 +1,6 @@
 import time
 import json
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
 from django.shortcuts import render
 
 from utils import quick_sort
@@ -8,6 +8,7 @@ from utils import quick_sort
 
 def index(request):
     return render(request, "index.html", {})
+
 
 def api(request):
     # This is the request to the '/api/sort' endpoint
@@ -17,10 +18,15 @@ def api(request):
         # we have to use request.body NOT request.POST, see url below
         # https://docs.djangoproject.com/en/3.1/ref/request-response/#django.http.HttpRequest.body
         result = json.loads(request.body.decode("utf-8"))
-        # simulate the delay of the response...
-        print("Waiting....")
-        time.sleep(2)
-        print("Go!")
-        return JsonResponse(quick_sort(result, 0, len(result) - 1), safe=False)
+        # Validation
+        # check if the payload is an array of strings
+        if isinstance(result, list) and all([isinstance(item, str) for item in result]):
+            # simulate the delay of the response...
+            print("Waiting....")
+            time.sleep(2)
+            print("Go!")
+            return JsonResponse(quick_sort(result, 0, len(result) - 1), safe=False)
+        else:
+            return HttpResponseBadRequest()
     else:
         return HttpResponse("<h1>This API endpoint only accepts POST requests</h1>")
